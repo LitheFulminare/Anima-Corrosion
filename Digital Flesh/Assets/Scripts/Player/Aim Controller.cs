303252630442778940d6ab7c4bde865d8c3861b5
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2025-02-02 14:35:16 -0300

    make character rotate when aiming

--- Digital Flesh/Assets/Scripts/Player/Aim Controller.cs
+++ Digital Flesh/Assets/Scripts/Player/Aim Controller.cs
@@ -6,12 +6,13 @@ using UnityEngine.InputSystem;
 
 public class AimController : MonoBehaviour
 {
-    [SerializeField] private InputActionReference _aim;
-    [SerializeField] private Camera _camera;
+    [SerializeField] private InputActionReference _aim;    
     [SerializeField] private Transform _defaultCameraPosition;
     [SerializeField] private Transform _aimingCameraPosition;
+    [SerializeField] private GameObject _characterSprite;
 
     private bool _isAiming = false;
+    private Camera _camera;
     private MovementController _movementController;
 
     private void OnEnable()
@@ -26,7 +27,13 @@ public class AimController : MonoBehaviour
 
     private void Start()
     {
+        _camera = Camera.main;
         _movementController = GetComponent<MovementController>();
+    }
+
+    private void Update()
+    {
+        if (_isAiming) LookAtMouse();
     }
 
     // changes camera position when RMB is pressed
@@ -34,14 +41,13 @@ public class AimController : MonoBehaviour
     {
         if (_isAiming)
         {
-            _camera.transform.position = _defaultCameraPosition.position;
-            _camera.transform.rotation = _defaultCameraPosition.rotation;
+            _camera.transform.SetPositionAndRotation(_defaultCameraPosition.position, _defaultCameraPosition.rotation);
+            _characterSprite.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
             _isAiming = false;
         }
         else
         {
-            _camera.transform.position = _aimingCameraPosition.position;
-            _camera.transform.rotation = _aimingCameraPosition.rotation;
+            _camera.transform.SetPositionAndRotation(_aimingCameraPosition.position, _aimingCameraPosition.rotation);
             _isAiming = true;
         }
 
@@ -50,5 +56,19 @@ public class AimController : MonoBehaviour
         {
             _movementController.SlowOnAim(_isAiming);
         }
+    }
+
+    // makes the character look to the crosshair position when aiming
+    private void LookAtMouse()
+    {
+        Vector3 mousePosition = _camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, _camera.transform.position.y - transform.position.y));
+
+        // calculates the angle to the mouse position
+        Vector3 direction = (mousePosition - transform.position).normalized;
+        float angleRad = Mathf.Atan2(-direction.x, direction.z); // Using x and z for 2.5D world
+        float angleDeg = Mathf.Rad2Deg * angleRad;
+
+        // subtracts -180 from the angle cuz the character was inverted                                                                
+        _characterSprite.transform.rotation = Quaternion.Euler(0f, angleDeg - 180, 0f);
     }
 }
